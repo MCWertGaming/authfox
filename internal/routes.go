306df55092bcf327892b5540ac9742d69ede1848
MCWertGaming <dleven@protonmail.com>
2022-01-31 16:14:07 +0000

    fixed login

--- internal/routes.go
+++ internal/routes.go
@@ -7,6 +7,6 @@ import (
 
 func SetRoutes(router *gin.Engine, collUsers, collVerify, collSession, collVerifySession, collProfiles *mongo.Collection) {
 	router.POST("/v1/register", registerUser(collUsers, collVerify, collSession, collVerifySession))
-	router.POST("/v1/login", loginUser(collUsers, collSession, collVerifySession, collVerify))
+	router.POST("/v1/login", loginUser(collUsers, collSession, collVerifySession, collVerify, collProfiles))
 	router.POST("/v1/verify", verifyUser(collVerifySession, collSession, collVerify, collProfiles, collUsers))
 }
